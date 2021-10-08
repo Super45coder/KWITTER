4f0e86542e1b0d28ddf2dbb92bb151d3cb11db46
Aarush Goradia <80135238+Super45coder@users.noreply.github.com>
2021-10-08 16:42:26 -0600

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,4 +1,20 @@
-audio1 = document.getElementById("welcome");
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyDM7gZQpHHs2UoXyv-F4Po9aHkzc2mFoLA",
+      authDomain: "kwitter-database-dc2be.firebaseapp.com",
+      databaseURL: "https://kwitter-database-dc2be-default-rtdb.firebaseio.com",
+      projectId: "kwitter-database-dc2be",
+      storageBucket: "kwitter-database-dc2be.appspot.com",
+      messagingSenderId: "1014999291327",
+      appId: "1:1014999291327:web:b1fd5986014a12461d3d0a"
+    };
+    
+    // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+views = 0;
+username = localStorage.getItem("Username");
+var audio1 = document.getElementById("welcome");
 function addUser(){
     username = document.getElementById("username").value;
 
@@ -10,4 +26,11 @@ function addUser(){
 
     console.log("Played");
   
+}
+function views(){
+likes = document.getElementById("views").value;
+updated_likes=Number(likes) + 1;
+firebase.database().ref(username).child(views).update({
+    likes:updated_likes
+    )};
 }
